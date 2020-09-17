719d9fdce30620060e7bd29aef20e34b2cc5ab97
Gajendiran, Gnanasekaran | RASIA <gnanasekaran.gajendi@rakuten.com>
2020-09-18 06:30:55 +0800

    M: deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ ls /tmp/repo && rm -rf /tmp/repo || true
 mkdir -p /tmp/repo
 cd /tmp/repo
 git clone git@github.com:ggnanasekaran77/ggnanasekaran.com.git
-cp -r ggnanasekaran.com/html/ /usr/share/nginx/html/
+cp -r ggnanasekaran.com/html/ /usr/share/nginx/
 cp ggnanasekaran.com/deploy.sh /root/deploy.sh && chmod 755 /root/deploy.sh
 
